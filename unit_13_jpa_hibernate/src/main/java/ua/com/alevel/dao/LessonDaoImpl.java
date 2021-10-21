dea6b00f4489b931abb55ee8b00e515d1ef64f12
d.danylenko <dinkadanko@gmail.com>
2021-10-22 00:11:02 +0300

    Commit - some refactoring and bug fixing

--- unit_13_jpa_hibernate/src/main/java/ua/com/alevel/dao/LessonDaoImpl.java
+++ unit_13_jpa_hibernate/src/main/java/ua/com/alevel/dao/LessonDaoImpl.java
@@ -8,6 +8,7 @@ import ua.com.alevel.entity.Student;
 
 import javax.persistence.TypedQuery;
 import java.time.Instant;
+import java.util.Optional;
 
 public class LessonDaoImpl implements LessonDao{
 
@@ -19,25 +20,30 @@ public class LessonDaoImpl implements LessonDao{
 
     @Override
     public LessonDto getNextLesson(long studentId){
-        LessonDto lessonDto = new LessonDto();
-        Lesson lesson = new Lesson();
         try(Session session = sessionFactory.openSession()){
             session.beginTransaction();
             TypedQuery<Student> query1 = session.createQuery("from Student s where id=:id", Student.class)
                     .setParameter("id", studentId);
             Student student = query1.getSingleResult();
             System.out.println(Instant.now().toString());
-            TypedQuery<Lesson> query2 = session.createQuery("from Lesson les where les.group = :group AND les.startDateTime > :datetime ", Lesson.class);
+            TypedQuery<Lesson> query2 = session.createQuery("from Lesson les where les.group = :group AND" +
+                    " les.startDateTime > :datetime order by les.startDateTime", Lesson.class);
             query2.setParameter("group", student.getGroup());
             query2.setParameter("datetime", Instant.now());
-            lesson = query2.getSingleResult();
-            lessonDto.setId(lesson.getId());
-            lessonDto.setGroup(lesson.getGroup().getGroupName());
-            lessonDto.setTopic(lesson.getTopic().getTopicName());
-            lessonDto.setLecturer(lesson.getLecturer().getFirstName() + " " + lesson.getLecturer().getLastName());
-            lessonDto.setStartDateTime(lesson.getStartDateTime());
-            lessonDto.setEndDateTime(lesson.getEndDateTime());
+            Optional<Lesson> foundLesson = query2.getResultStream().findAny();
+            if(foundLesson.isPresent()){
+                LessonDto lessonDto = new LessonDto();
+                lessonDto.setId(foundLesson.get().getId());
+                lessonDto.setGroup(foundLesson.get().getGroup().getGroupName());
+                lessonDto.setTopic(foundLesson.get().getTopic().getTopicName());
+                lessonDto.setLecturer(foundLesson.get().getLecturer().getFirstName()
+                        + " " + foundLesson.get().getLecturer().getLastName());
+                lessonDto.setStartDateTime(foundLesson.get().getStartDateTime());
+                lessonDto.setEndDateTime(foundLesson.get().getEndDateTime());
+                return lessonDto;
+            }else{
+                return new LessonDto();
+            }
         }
-        return lessonDto;
     }
 }
